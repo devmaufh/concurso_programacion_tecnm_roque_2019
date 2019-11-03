d20b6d1d82beba47e9a290db3bab1bda35b0ac2c
Mauricio Flores Hernandez <mau1361317@gmail.com>
2019-11-02 19:41:07 -0600

    Algoritmos concurso de programacion

--- 5.py
+++ 5.py
@@ -2,7 +2,7 @@ x = input("Ingresa una operacion ")
 operadores = []
 operandos  = []
 for i in x:
-    if(i.isnumeric()):
+    if(i.isnumeric() or i.isalpha()):
         operandos.append(i)
     else:
         if not i.isspace() and i is not "(" and i is not ")": operadores.append(i)
